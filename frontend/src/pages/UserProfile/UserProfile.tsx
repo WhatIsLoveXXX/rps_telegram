99a5733b12e520a5f63192cebad6c31efb6b8d33
vlad <vladyslav.zhytnyk@gmail.com>
2025-06-06 23:07:40 +0300

    add wallet operations

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,14 +1,69 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Page } from "@/components/Page.tsx";
 import { getUsers } from "../../../services/users.api";
 import { initData, useSignal } from "@telegram-apps/sdk-react";
+import {
+  TonConnectButton,
+  useTonAddress,
+  useTonConnectUI,
+  useTonWallet,
+} from "@tonconnect/ui-react";
+import { topUpBalance, withdrawBalance } from "../../../services/balance.api";
+import { prepareTransaction } from "@/utils/transactions/prepareTransaction";
+import { getTxByBOC } from "@/utils/transactions/getTxByBoc";
 
 export const UserProfile: FC = () => {
-  const initDataRaw = useSignal(initData.raw);
+  const wallet = useTonWallet();
+  const [tonConnectUI] = useTonConnectUI();
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
+  const [topUpAmount, setTopUpAmount] = useState(0);
+  const userFriendlyAddress = useTonAddress();
+
+  const handleTopUp = async () => {
+    const transaction = prepareTransaction(topUpAmount, "Mock user name");
+    try {
+      const transactionStatus = await tonConnectUI.sendTransaction(transaction);
+      await topUpBalance(topUpAmount, transactionStatus.boc);
+    } catch (e) {
+      console.log(e);
+    }
+    // const tx = await getTxByBOC(transactionStatus.boc, userFriendlyAddress);
+  };
+
   return (
     <Page back={false}>
       <p>UserProfile</p>
-      <button onClick={() => getUsers(initDataRaw)}>/api/users</button>
+      <div className="pb-10">
+        <div className="flex justify-between pt-2 px-1">
+          {!wallet ? (
+            <TonConnectButton className="ton-connect-page__button !m-0" />
+          ) : (
+            <TonConnectButton className="ton-connect-page__button-connected !m-0" />
+          )}
+        </div>
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Top up amount"
+          className="bg-white text-black"
+          value={topUpAmount}
+          onChange={(e) => setTopUpAmount(Number(e.target.value))}
+        />
+        <button onClick={async () => await handleTopUp()}>Top up</button>
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Withdraw amount"
+          className="bg-white text-black"
+          value={withdrawAmount}
+          onChange={(e) => setWithdrawAmount(Number(e.target.value))}
+        />
+        <button onClick={async () => await withdrawBalance(withdrawAmount)}>
+          Withdraw
+        </button>
+      </div>
     </Page>
   );
 };
--- frontend/src/utils/transactions/prepareTransaction.ts
+++ frontend/src/utils/transactions/prepareTransaction.ts
@@ -4,12 +4,9 @@ import { beginCell } from "@ton/ton";
 const tonToNano = (tonAmount: number) => Math.round(tonAmount * 1_000_000_000);
 
 export const prepareTransaction = (
-  priceInUsdt: number,
   tonPrice: number,
   userName: string
 ): SendTransactionRequest => {
-  const tonAmount = priceInUsdt / tonPrice;
-
   const body = beginCell()
     .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
     .storeStringTail(`Transaction sent from ${userName}`) // write our text comment
@@ -20,7 +17,7 @@ export const prepareTransaction = (
     messages: [
       {
         address: import.meta.env.VITE_RECEIVER_ADDRESS, // TON
-        amount: tonToNano(tonAmount).toString(), // Toncoin in nanotons
+        amount: tonToNano(tonPrice).toString(), // Toncoin in nanotons
         payload: body.toBoc().toString("base64"),
         // amount: "2000" //test
       },
